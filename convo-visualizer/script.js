1820d8ef5710c29435702e4c6afdeb1adb00e656
nifled <erickdelfinmolina@gmail.com>
2019-01-22 02:54:15 -0700

    c-v | Create yearly message bar graph

--- convo-visualizer/script.js
+++ convo-visualizer/script.js
@@ -1,4 +1,4 @@
-fetch("dania.json")
+fetch("manny.json")
   .then(res => res.json())
   .then(data => {
     start(data);
@@ -31,6 +31,8 @@ function start(data) {
   const aDaysObj = functions.groupByDay(a, "timestamp_ms");
   const bDaysObj = functions.groupByDay(b, "timestamp_ms");
 
+  // ----------------------------------------------------------------
+  // ---------------------- General section -------------------------
   // Total messages
   d3.select(PERSON_1_MSGS).text(a.length.toLocaleString());
   d3.select(PERSON_2_MSGS).text(b.length.toLocaleString());
@@ -46,143 +48,95 @@ function start(data) {
   let bDay = new Date(functions.mostActiveDay(bDaysObj));
   d3.select(PERSON_1_ACTY).text(aDay.toISOString().split("T")[0]);
   d3.select(PERSON_2_ACTY).text(bDay.toISOString().split("T")[0]);
+  // ----------------------------------------------------------------
+  // ----------------------------------------------------------------
 
-  console.log(aDaysObj);
-  Object.keys(aDaysObj).forEach(dayKey => {
-    const ms = parseInt(dayKey);
-    console.log(new Date(ms).getFullYear());
-  });
+  const years = functions.groupByYear(messages, "timestamp_ms");
+  console.log(years);
+  console.log(a.length);
+
+  // ----------------------------------------------------------------
+  // ----------------------------------------------------------------
+  // CHARTS
+  const margin = 60;
+  const width = 1000 - 2 * margin;
+  const height = 600 - 2 * margin;
+
+  const svg = d3.select("svg");
+  const chart = svg
+    .append("g")
+    .attr("transform", `translate(${margin}, ${margin})`);
+
+  // X and Y axis
+  const yScale = d3
+    .scaleLinear()
+    .range([height, 0])
+    .domain([0, d3.max(Object.keys(years).map(x => years[x].length))]);
+
+  const xScale = d3
+    .scaleBand()
+    .range([0, width])
+    .domain(Object.keys(years).map(s => s))
+    .padding(0.2);
+
+  chart.append("g").call(d3.axisLeft(yScale));
+
+  chart
+    .append("g")
+    .attr("transform", `translate(0, ${height})`)
+    .call(d3.axisBottom(xScale));
+
+  // Fill bars
+  chart
+    .selectAll()
+    .data(Object.keys(years))
+    .enter()
+    .append("rect")
+    .attr("class", "bar")
+    .attr("x", s => xScale(s))
+    .attr("y", s => yScale(years[s].length))
+    .attr("height", s => height - yScale(years[s].length))
+    .attr("width", xScale.bandwidth());
+
+  // Grid lines - horizontal
+  chart
+    .append("g")
+    .attr("class", "grid")
+    .call(
+      d3
+        .axisLeft()
+        .scale(yScale)
+        .tickSize(-width, 0, 0)
+        .tickFormat("")
+    );
+
+  // Labels
+  svg
+    .append("text")
+    .attr("x", -(height / 2) - margin)
+    .attr("y", margin / 5)
+    .attr("transform", "rotate(-90)")
+    .attr("text-anchor", "middle")
+    .text("Messages Sent");
+
+  svg
+    .append("text")
+    .attr("class", "label")
+    .attr("x", width / 2 + margin)
+    .attr("y", height + margin * 1.8)
+    .attr("text-anchor", "middle")
+    .text("Years");
+
+  svg
+    .append("text")
+    .attr("class", "title")
+    .attr("x", width / 2 + margin)
+    .attr("y", 40)
+    .attr("text-anchor", "middle")
+    .text("Messages sent by Year");
+  // ----------------------------------------------------------------
+  // ----------------------------------------------------------------
 }
-
-const sample = [
-  {
-    language: "Rust",
-    value: 78.9,
-    color: "#000000"
-  },
-  {
-    language: "Kotlin",
-    value: 75.1,
-    color: "#00a2ee"
-  },
-  {
-    language: "Python",
-    value: 88.0,
-    color: "#fbcb39"
-  },
-  {
-    language: "TypeScript",
-    value: 67.0,
-    color: "#007bc8"
-  },
-  {
-    language: "Go",
-    value: 65.6,
-    color: "#65cedb"
-  },
-  {
-    language: "Swift",
-    value: 65.1,
-    color: "#ff6e52"
-  },
-  {
-    language: "JavaScript",
-    value: 61.9,
-    color: "#f9de3f"
-  },
-  {
-    language: "C#",
-    value: 60.4,
-    color: "#5d2f8e"
-  },
-  {
-    language: "F#",
-    value: 59.6,
-    color: "#008fc9"
-  },
-  {
-    language: "Clojure",
-    value: 59.6,
-    color: "#507dca"
-  }
-];
-
-const margin = 60;
-const width = 1000 - 2 * margin;
-const height = 600 - 2 * margin;
-
-const svg = d3.select("svg");
-const chart = svg
-  .append("g")
-  .attr("transform", `translate(${margin}, ${margin})`);
-
-// X and Y axis
-const yScale = d3
-  .scaleLinear()
-  .range([height, 0])
-  .domain([0, 100]);
-
-const xScale = d3
-  .scaleBand()
-  .range([0, width])
-  .domain(sample.map(s => s.language))
-  .padding(0.2);
-
-chart.append("g").call(d3.axisLeft(yScale));
-
-chart
-  .append("g")
-  .attr("transform", `translate(0, ${height})`)
-  .call(d3.axisBottom(xScale));
-
-// Fill bars
-chart
-  .selectAll()
-  .data(sample)
-  .enter()
-  .append("rect")
-  .attr("class", "bar")
-  .attr("x", s => xScale(s.language))
-  .attr("y", s => yScale(s.value))
-  .attr("height", s => height - yScale(s.value))
-  .attr("width", xScale.bandwidth());
-
-// Grid lines - horizontal
-chart
-  .append("g")
-  .attr("class", "grid")
-  .call(
-    d3
-      .axisLeft()
-      .scale(yScale)
-      .tickSize(-width, 0, 0)
-      .tickFormat("")
-  );
-
-// Labels
-svg
-  .append("text")
-  .attr("x", -(height / 2) - margin)
-  .attr("y", margin / 2.4)
-  .attr("transform", "rotate(-90)")
-  .attr("text-anchor", "middle")
-  .text("Love meter (%)");
-
-svg
-  .append("text")
-  .attr("class", "label")
-  .attr("x", width / 2 + margin)
-  .attr("y", height + margin * 1.8)
-  .attr("text-anchor", "middle")
-  .text("Languages");
-
-svg
-  .append("text")
-  .attr("class", "title")
-  .attr("x", width / 2 + margin)
-  .attr("y", 40)
-  .attr("text-anchor", "middle")
-  .text("Most loved programming languages in 2018");
 
 // Interactivity
 // svg
